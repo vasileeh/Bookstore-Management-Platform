4bcd77f22fd760a7231587115b88e026ae3532e5
vasileeh <vasiletirnovan004@gmail.com>
2025-05-05 10:51:10 +0300

    Genre repository

---
+++ src/main/java/com/example/bookstore/repository/GenreRepository.java
@@ -0,0 +1,10 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+    Optional<Genre> findByName(String name);
+}
