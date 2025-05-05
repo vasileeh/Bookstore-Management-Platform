bb7c61a79093067d288f8b259c937b1ace5f2c5b
vasileeh <vasiletirnovan004@gmail.com>
2025-05-05 13:28:05 +0300

    Book service

---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,43 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public Book createBook(Book book){
+        return bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    public Book getBooksById(Long id){
+        return bookRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("The book was not found"));
+    }
+
+    public Book updateBook(Long id, Book updateBook){
+        Book existing=getBooksById(id);
+        existing.setTitle(updateBook.getTitle());
+        existing.setGenre(updateBook.getGenre());
+        existing.setAuthor(updateBook.getAuthor());
+        existing.setPrice(updateBook.getPrice());
+        existing.setStock(updateBook.getStock());
+
+        return bookRepository.save(existing);
+    }
+
+    public void deleteBook(Long id){
+        bookRepository.deleteById(id);
+    }
+}
