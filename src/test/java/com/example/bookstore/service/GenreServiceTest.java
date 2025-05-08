65ea80a1dd4cda0d00fc62ca6244b7c5dfaa9a0e
vasileeh <vasiletirnovan004@gmail.com>
2025-05-08 11:05:52 +0300

    GenreService Test

---
+++ src/test/java/com/example/bookstore/service/GenreServiceTest.java
@@ -0,0 +1,83 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.service.GenreService;
+import com.example.bookstore.model.Genre;
+import com.example.bookstore.repository.GenreRepository;
+import org.h2.command.dml.Update;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class GenreServiceTest {
+    @Mock
+    private GenreRepository genreRepository;
+
+    @InjectMocks
+    private GenreService genreService;
+
+    private Genre genre;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+
+        genre=new Genre();
+        genre.setId(1L);
+        genre.setName("Test Genre");
+        genre.setDescription("Test description");
+        genre.setBooks(new ArrayList<>());
+    }
+
+    @Test
+    void testCreateGenre(){
+        when(genreRepository.save(genre)).thenReturn(genre);
+
+        Genre saved=genreService.createGenre(genre);
+
+        assertEquals("Test Genre",saved.getName());
+        verify(genreRepository, times(1)).save(genre);
+    }
+
+    @Test
+    void testGetAllGenres(){
+        List<Genre> genres=List.of(genre);
+        when(genreRepository.findAll()).thenReturn(genres);
+
+        List<Genre> result=genreService.getAllGenres();
+
+        assertEquals(1, result.size());
+        verify(genreRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testGetGenreById_Found(){
+        when(genreRepository.findById(1L)).thenReturn(Optional.of(genre));
+
+        Genre result=genreService.getGenreById(1L);
+
+        assertEquals("Test Genre",result.getName());
+    }
+
+    @Test
+    void testGetGenreById_NotFound(){
+        when(genreRepository.findById(2L)).thenReturn(Optional.empty());
+
+        RuntimeException exception=assertThrows(RuntimeException.class,
+                ()->genreService.getGenreById(2L));
+        assertEquals("The genre was not found",exception.getMessage());
+    }
+
+    @Test
+    void testGetGenreByName_Found(){
+        when(genreRepository.findByName("Test Genre")).thenReturn(Optional.of(genre));
+
+        Genre result=genreService.getGenreByName("Test Genre");
+
+        assertEquals("Test Genre", result.getName());
+    }
+}
